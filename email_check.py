502dfaecb4a319a3e448c3e906623c5367c2511d
Dmitry <loldragonmovies@gmail.com>
2021-03-29 02:34:59 +0300

    Well done

--- email_check.py
+++ email_check.py
@@ -1,24 +1,28 @@
 import re
 
 
+# Букавы, нижние подчеркивания, цифры, точки (но чтобы не заканчивалось на точку и нижнее подчеркивание)
 def check_left_part(l_part):
-    found = re.search(r"^[a-bA-B][a-bA-B0-9_.]*$", l_part)
-    if found == l_part:
+    found_str = re.fullmatch(r"^[a-zA-Z](?:[a-zA-Z\d_.][a-zA-Z\d])*(?:[a-zA-Z\d])*$", l_part)
+    if found_str is not None and found_str.string == l_part:
         return True
     return False
 
 
+# тут просто списки по 5 элементов
 def check_right_part(r_part):
     split_parts = str.split(r_part, ".")
     try:
         tld = split_parts.pop()
     except IndexError:
         return False
-    if re.search(r"^(?:com|ru|org|net|su)$", tld) == tld and len(split_parts) != 0:
+    matched_tld = re.fullmatch(r"^(?:com|ru|org|net|su)$", tld)
+    if matched_tld is not None and matched_tld.string == tld and len(split_parts) != 0:
         sld = split_parts.pop()
         while len(split_parts) != 0:
-            sld += split_parts.pop()
-        if re.search(r"^(?:stud.kpfu|mail|gmail|inbox|wikipedia)$", tld) == tld:
+            sld = split_parts.pop() + sld
+        matched_sld = re.fullmatch(r"^(?:stud.kpfu|mail|gmail|inbox|wikipedia)$", sld)
+        if matched_sld is not None and matched_sld.string == sld:
             return True
     return False
 
